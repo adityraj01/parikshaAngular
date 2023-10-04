f533fac4c4e5f0f158db72c15de07a9295c059de
Adity Shaw <adityshaw99@gmail.com>
2023-10-04 23:02:34 +0530

    commit

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://pariksha-production-bf1b.up.railway.app';
+// export const baseUrl = 'https://pariksha-production-bf1b.up.railway.app';
+export const baseUrl = 'http://localhost:8080';
